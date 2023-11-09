8cb898e45d77aa6b873a6bbcbfa7a44ac7f5fd75
oncoapop <oncoapop@sdf.org>
2023-11-09 00:47:39 -0800

    Update install R

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Install the dependencies
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-get install libcurl4-openssl-dev
 
 # Add the CRAN repository to your system sources’ list
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
@@ -10,7 +11,7 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 echo "deb [signed-by=/usr/share/keyrings/r-project.gpg] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee -a /etc/apt/sources.list.d/r-project.list
 
 sudo apt update
-sudo apt install --no-install-recommends r-base
+sudo apt install r-base
 
 # Check version
 R --version
